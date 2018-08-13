3d579705b5c77824ae0b2e1e1b84989bea831139
Jamie Brown <jmbrowncow@gmail.com>
2018-08-13 18:22:53 +0000

    automatically backed up by learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -31,6 +31,12 @@ class OwnersController < ApplicationController
   end
 
   post '/owners/:id' do
-    binding.pry
+    @owner = Owner.find(params[:id])
+    @owner.update(params[:owner])
+    if !params["pet"]["name"].empty?
+      @owner.pets.build(name: params["pet"]["name"])
+    end
+    @owner.save
+    redirect "owners/#{@owner.id}"
   end
 end
